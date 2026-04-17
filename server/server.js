ac7d3b19d9ae01676681d0c0a609b398460d2950
Saimon Islam <118347533+Saimon1831552@users.noreply.github.com>
2026-04-17 21:05:39 +0600

    Update server.js

--- server/server.js
+++ server/server.js
@@ -36,7 +36,7 @@ app.use((err, req, res, next) => {
   res.status(500).json({ success: false, message: 'Internal server error', error: err.message });
 });
 
-const PORT = process.env.PORT || 5000 || 8080;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 module.exports = app;
