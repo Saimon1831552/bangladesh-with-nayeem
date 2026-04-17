034ea8b654d552bfe5f9b8abba5dd58215bebd55
Saimon1831552 <saimon.islam@northsouth.edu>
2026-04-17 20:09:27 +0600

    update DB connection

--- server/db.js
+++ server/db.js
@@ -3,11 +3,11 @@ require('dotenv').config();
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: process.env.MYSQLHOST || process.env.DB_HOST,
-  user: process.env.MYSQLUSER || process.env.DB_USER,
-  password: process.env.MYSQLPASSWORD || process.env.DB_PASSWORD,
-  database: process.env.MYSQLDATABASE || process.env.DB_NAME,
-  port: process.env.MYSQLPORT || process.env.DB_PORT || 3306,
+  host: process.env.MYSQLHOST || process.env.DB_HOST || nozomi.proxy.rlwy.net,
+  user: process.env.MYSQLUSER || process.env.DB_USER || root,
+  password: process.env.MYSQLPASSWORD || process.env.DB_PASSWORD || boMKjNJmgoEcGjpeUlGJqFOcVdiRPDti,
+  database: process.env.MYSQLDATABASE || process.env.DB_NAME || railway,
+  port: process.env.MYSQLPORT || process.env.DB_PORT || 3306 || 47122,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
