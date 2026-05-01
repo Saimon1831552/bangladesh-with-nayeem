0afe570e46ee73db10362bda6e152048c235e8a8
Saimon1831552 <saimon.islam@northsouth.edu>
2026-05-01 22:03:17 +0600

    update

--- src/app/blogs/page.js
+++ src/app/blogs/page.js
@@ -3,7 +3,7 @@ import Blog from './blog';
 
 export const dynamic = 'force-dynamic';
 
-const API_BASE = (process.env.RAILWAY_PUBLIC_DOMAIN)
+const API_BASE = (process.env.NEXT_PUBLIC_API_URL)
   .replace(/\/api\/?$/, '');
 
 export default async function BlogsPage() {
