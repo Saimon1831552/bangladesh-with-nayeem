d830e826aa70d3c2960c6d8921011dbf1f5300fd
Saimon1831552 <saimon.islam@northsouth.edu>
2026-05-16 20:54:36 +0600

    update logo

--- src/components/shared/Navber/navber.jsx
+++ src/components/shared/Navber/navber.jsx
@@ -20,7 +20,7 @@ export default function Navbar() {
         <Link href="/">
           <div className=' overflow-hidden'>
             <img
-              src='https://res.cloudinary.com/dx4o0i6c2/image/upload/f_auto,q_auto/bangladesh-with-naim-logo_pubs5n'
+              src='https://res.cloudinary.com/dx4o0i6c2/image/upload/f_auto,q_auto/bangladesh-with-naim-logo_o5c6z1'
               className='h-14 rounded-sm'
               alt="Bangladesh with Naim"
             />
