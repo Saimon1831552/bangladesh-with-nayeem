f3d3ea05fcce708e99842274fabe20143a375888
Saimon Islam <118347533+Saimon1831552@users.noreply.github.com>
2026-07-02 11:19:10 +0600

    Improve error handling in generateMetadata function
    Refactor generateMetadata to handle errors and check response data.

--- src/app/blogs/[slug]/page.jsx
+++ src/app/blogs/[slug]/page.jsx
@@ -1,32 +1,36 @@
 import Blogclient from "./Blogclient";
 
 export async function generateMetadata({ params }) {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/api/blogs/${params.slug}`,
-    {
-      next: { revalidate: 3600 },
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/api/blogs/${params.slug}`,
+      {
+        next: { revalidate: 3600 },
+      }
+    );
+
+    if (!res.ok) {
+      return {};
     }
-  );
 
-  const { data } = await res.json();
+    const json = await res.json();
 
-  return {
-    title: data.title,
-    description: data.excerpt,
-    openGraph: {
-      title: data.title,
-      description: data.excerpt,
-      images: [
-        {
-          url: data.image_url,
-          width: 1200,
-          height: 630,
-        },
-      ],
-    },
-  };
+    if (!json?.data) {
+      return {};
+    }
+
+    return {
+      title: json.data.title,
+      description: json.data.excerpt,
+      openGraph: {
+        images: [json.data.image_url],
+      },
+    };
+  } catch (err) {
+    console.error(err);
+    return {};
+  }
 }
-
 export default function Page() {
   return <Blogclient />;
 }
