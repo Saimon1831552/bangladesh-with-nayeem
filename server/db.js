03eca247dd28f00441fcb9461b6e29b4901e8585
Saimon Islam <118347533+Saimon1831552@users.noreply.github.com>
2026-08-02 16:39:48 +0600

    Remove default port fallback in db configuration

--- server/db.js
+++ server/db.js
@@ -7,7 +7,7 @@ const pool = mysql.createPool({
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE,
-  port: parseInt(process.env.MYSQLPORT) || 3306,
+  port: parseInt(process.env.MYSQLPORT),
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
