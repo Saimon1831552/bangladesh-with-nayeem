760b18e5351cceac73ec7211db9412920beb3eaa
Saimon Islam <118347533+Saimon1831552@users.noreply.github.com>
2026-09-22 12:39:13 +0600

    Comment out express setup in booking.js
    Comment out express import and router initialization.

--- server/routes/booking.js
+++ server/routes/booking.js
@@ -1,4 +1,4 @@
-const express = require('express');
+/*const express = require('express');
 const router = express.Router();
 const nodemailer = require('nodemailer');
 
@@ -76,3 +76,4 @@ router.post('/', async (req, res) => {
 });
 
 module.exports = router;
+*/
